0c39f75ea10b0797ab197bfd12ea22ef8a7ec2ee
Shigui Li <141310532+ShiguiLi@users.noreply.github.com>
2023-09-01 15:02:21 +0800

    Update sample.sh

--- sample.sh
+++ sample.sh
@@ -12,14 +12,14 @@ data="cifar10"
 # data="bedroom_guided"
 # data="celeba"
 # scale="1"
-# sampleMethod='dpm'
-# sampleMethod='fde'
-sampleMethod="rde"
-# type="ei"
-type="scire"
-STEPLIST=('100')
-# Trajectory="logSNR"
-Trajectory="NSR"
+# type='dpm'
+# type='fde'
+type="rde"
+#sampleMethod="ei"
+sampleMethod="scire"
+STEPLIST=('12')
+Trajectory="logSNR"
+# Trajectory="NSR"
 # Trajectory="time_uniform"
 order="2"
 endtime="1e-4"
@@ -31,7 +31,7 @@ method="singlestep_fixed"
 for steps in ${STEPLIST[*]};
 do
     workdir="experiments/"$data"/"$sampleMethod"_"$method"_order"$order"_"$steps"_"$Trajectory"_"$type"_"$endtime
-#workdir="experiments/"$data"/"$sampleMethod"_"$method"_order"$order"_"$steps"_"$Trajectory"_type_"$type_scale"$scale"
+#workdir="experiments/"$data"/"$sampleMethod"_"$method"_order"$order"_"$steps"_"$DIS"_type_"$type_scale"$scale"
     CUDA_VISIBLE_DEVICES=$DEVICES python main.py --config $data".yml" --exp=$workdir --sample --fid --timesteps $steps --eta 0 --ni --skip_type=$Trajectory --sample_type=$sampleMethod --scire_solver_order=$order --end_time=$endtime  --scire_solver_method=$method --scire_solver_type=$type --port 12355 #--scale=$scale
 done
 
